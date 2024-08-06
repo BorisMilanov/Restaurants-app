44ee259584120349f55089bf5d3f90f9df7255ff
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2024-08-06 19:48:37 +0300

    Update AuthContext.jsx

--- client/src/Auth/AuthContext.jsx
+++ client/src/Auth/AuthContext.jsx
@@ -1,10 +1,32 @@
 import React, { createContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext({
+    userId: '',
     email: '',
     token: '',
     isAuthenticated: false,
-    changeAuthState: () => null
+    changeAuthState: (authState = {}) => null
 });
 
+export function AuthContextProvider(props) {
+    const [authState, setAuthState] = useState({})
+    const changeAuthState = (state) => {
+        localStorage.setItem('accessToken', state.accessToken)
+        setAuthState(state)
+      }
+    
+    const contextData ={
+        userId: authState._id,
+        email: authState.email,
+        token: authState.token,
+        isAuthenticated:!!authState.email,
+        changeAuthState
+     }
+    return (
+        <AuthContext.Provider value={contextData}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+// add user id
 export default AuthContext;
